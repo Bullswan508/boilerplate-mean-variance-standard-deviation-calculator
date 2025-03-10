9c915644580a1a1f36a7485733c795ec98b03c67
Bullswan508 <36522657+Bullswan508@users.noreply.github.com>
2025-03-10 14:13:50 -0400

    Added in the code from boilerplate repo

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,7 +2,29 @@ import numpy as np
 
 def calculate(list):
 
+    if len(list) != 9:
+        raise ValueError("List must contain nine numbers.")
+
+    calculations = {}
+
+    list_flattened = np.array(list, np.int32)
+
+    list_matrix = list_flattened.reshape(3,3)
+
+    calculations['mean'] = [np.mean(list_matrix, axis=0).tolist(), np.mean(list_matrix, axis=1).tolist(), np.mean(list_flattened)]
+    calculations['variance'] = [np.var(list_matrix, axis=0).tolist(), np.var(list_matrix, axis=1).tolist(), np.var(list_flattened)]
+    calculations['standard deviation'] = [np.std(list_matrix, axis=0).tolist(), np.std(list_matrix, axis=1).tolist(), np.std(list_flattened)]
+    calculations['max'] = [np.max(list_matrix, axis=0).tolist(), np.max(list_matrix, axis=1).tolist(), np.max(list_flattened)]
+    calculations['min'] = [np.min(list_matrix, axis=0).tolist(), np.min(list_matrix, axis=1).tolist(), np.min(list_flattened)]
+    calculations['sum'] = [np.sum(list_matrix, axis=0).tolist(), np.sum(list_matrix, axis=1).tolist(), np.sum(list_flattened)]
 
 
 
     return calculations
+
+
+if __name__ == "__main__":
+    
+    nums = [0,1,2,3,4,5,6,7,8]
+    
+    print(calculate(nums))
